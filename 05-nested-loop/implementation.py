a81f52a79d9d71fc68a6ad813ba08c52fae52f63
MatthiasCr <matthias.cram@gmail.com>
2025-11-03 18:23:14 +0100

    05-nested-loop

--- 05-nested-loop/implementation.py
+++ 05-nested-loop/implementation.py
@@ -11,4 +11,35 @@ def join(prepared: dict) -> List[Tuple[Point, Point]]:
     result = []
     rtree_a = prepared["rtree_a"]
     points_b = prepared["points_b"]
+
+    for point_b in points_b:
+        # range query
+        points_a = rangeQuery(rtree_a.root, point_b)
+        for point_a in points_a:
+            result.append((point_a, point_b))
+
     return result
+
+
+def rangeQuery(node: Node, query: Point) -> List[Point]:
+    childrenToSearch: List[Node] = []
+    results: List[Point] = []
+
+    for child in node.children:
+        if child.mbr.intersects(query.mbr):
+            childrenToSearch.append(child)
+    
+    if node.is_leaf():
+        for point in childrenToSearch:
+            if point.mbr.intersects(query.mbr):
+                results.append(point)
+        return results
+
+    # non-leaf so do recursion in subtree
+    for child in childrenToSearch:
+        recresult = rangeQuery(child, query)
+        results.extend(recresult)
+
+    return results
+
+    
